0068311152efc77708fb05e74754390c068073df
giangh00 <162215746+giangh00@users.noreply.github.com>
2024-07-22 05:25:55 +1200

    Initial commit

---
+++ src/Components/Navbar/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from "../Navbars/Navbar";
+
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
---
+++ src/Components/Navbar/pages/Pages.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Home from './Home'
+const pages = () => {
+  return (
+    <div>pages</div>
+  )
+}
+
+export default Pages
